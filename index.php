45606d011b823fa731cddb07bb2a66a01fdd9d31
sjackson01 <stevenpjackson0@gmail.com>
2020-02-27 07:29:37 -0600

    Add (index.php>Hunk 1) : Create basic home page that displays information about images stored within a direcotry. Add (show_image.php> Hunk 1) : Create file that handles the header redirect. Add (functions.js) : Move functions.js into a subfolder.

--- index.php
+++ index.php
@@ -1,46 +1,47 @@
-<?php # Script 11.5 - show image.php
-// This page displays an image
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <titl>Images</titl>
+    <script charset='UTF-8' src="js/functions.js"></script>
+</head>
+<body>
+<p>Click on an image to view it in a seperate window.</p>
+<ul>
+<?php # Script 11.6 - images.php
+    // This script list the imaes in the uploads directory.
+    // This verison how shows each image's file size and uploaded date and time.
 
-$name = FALSE; // Flag variable
+    // Set the  default  timezone:
+    date_default_timezone_get('Amercian/Chicago');
+    
+    $dir = 'images/'; // Define the directory to view
+    $files = scandir($dir); // Read all the the images into an array
 
-// Check for an image name in the URL 
-if(isset($_GET['image'])){
+    // Dispaly each image caption as a link to the JavaScript function: 
+    foreach ($files as $image){
+        if (substr($image, 0, 1) != '.') { // Ignore anything starting with a period 
 
-    // Make sure it has an image's extension
-    $ext = strtolower( substr ($_GET['image'], -4));
+            // Get the image's size in pixels:
+            $image_size = getimagesize("$dir/$image");
 
-    if (($ext == '.jpg') OR ($ext == '.jpeg') OR ($ext == '.png')) {
+            // Calculate the image's  size in kilobytes:
+            $file_size = round( (filesize("$dir/$image")) / 1024) . "kb";
 
-        // Full image path: 
-        $image = "../uploads/{$_GET['image']}";
+            // Determine the image's upload date and time:
+            $image_date = date("F d, Y H:i:s", filemtime("$dir/$image"));
 
-        // Check that the image exists and is a file: 
-        if (file_exists($image) && (is_file($image))) {
+            // Make the image's URL-safe:
+            $image_name = urlencode($image);
 
-            // Set the name as this iamge: 
-            $name = $_GET['image'];
+            // Print information
+            echo "<li><a href=\"javascript:create_window('$image_name',$image_size[0],
+            $image_size[1])\">$image</a> $file_size ($image_date)</li>\n";
 
-        } // End of file_exists() IF. 
-    } // End of $ext IF. 
-} // End of isset ($_GET['image']) IF.
-
-// If there was a pborlem, use the defaut image:
-
-if(!$name){
-    $image = 'images/unavaible.png';
-    $name = 'unavailable.png';
-}
-
-// Get the image information: 
-$info = getimagesize($image);
-$fs = filesize($image);
-
-// Send the content information
-header ("Content-Type: {$info['mime']} \n");
-header ("Content-Disposition: inline; filename=\"name\"\n");
-header ("Content-Length: $fs\n");
-
-//Send the file
-readfile($image);
+        } // End of the IF. 
+    } // End of the foreach loop. 
 
 ?>
+</ul>
+</body>
+</html>
